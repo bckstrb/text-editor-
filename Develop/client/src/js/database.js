2ed16ff6c3b473fe4cfdd983b6777a0ceb6d9e40
bckstrb <rbccstrb@gmail.com>
2022-11-23 19:10:17 -0700

    fixed bugs

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -28,7 +28,8 @@ export const getDb = async () => {console.log('getDb');
 const jateDb = await openDB('jate', 1);
 const tx = jateDb.transaction('jate', 'readonly');
 const store = tx.objectStore('jate');
-const request = store.getOne(1);
+console.log(store);
+const request = store.get(1);
 const result = await request;
 result ? console.log('retreive from DB', result.value): console.log('Data not found');
 return result?.value
